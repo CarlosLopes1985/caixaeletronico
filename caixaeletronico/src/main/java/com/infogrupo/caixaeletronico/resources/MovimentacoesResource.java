ef45d589c4fbea13afaa0ebd29f8dc2616bc52db
Carlos Lopes <Carlos Lopes@DESKTOP-NQQH900.lan>
2019-11-21 01:19:43 -0200

    Implementação classe de movimentaçoes parte 1

--- caixaeletronico/src/main/java/com/infogrupo/caixaeletronico/resources/MovimentacoesResource.java
+++ caixaeletronico/src/main/java/com/infogrupo/caixaeletronico/resources/MovimentacoesResource.java
@@ -1,8 +1,29 @@
 package com.infogrupo.caixaeletronico.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.infogrupo.caixaeletronico.entity.Movimentacoes;
+import com.infogrupo.caixaeletronico.services.MovimentacoesService;
+
 @RestController
+@RequestMapping(value="/movimentacoes")
 public class MovimentacoesResource {
 
+	@Autowired
+	private MovimentacoesService movimentacoesService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Movimentacoes>> findAllContas(){
+		
+		List<Movimentacoes>contas = movimentacoesService.buscarTodas();
+	
+		return ResponseEntity.ok().body(contas);
+		
+	}
 }
--- caixaeletronico/src/main/java/com/infogrupo/caixaeletronico/services/MovimentacoesService.java
+++ caixaeletronico/src/main/java/com/infogrupo/caixaeletronico/services/MovimentacoesService.java
@@ -1,8 +1,13 @@
 package com.infogrupo.caixaeletronico.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.infogrupo.caixaeletronico.entity.Conta;
+import com.infogrupo.caixaeletronico.entity.Movimentacoes;
+import com.infogrupo.caixaeletronico.repository.ContaRepository;
 import com.infogrupo.caixaeletronico.repository.MovimentacoesRepository;
 
 @Service
@@ -11,4 +16,24 @@ public class MovimentacoesService {
 	@Autowired
 	private MovimentacoesRepository movimentacoesRepository;
 	
+	@Autowired
+	private ContaRepository contaRepository;
+	
+	public List<Movimentacoes>buscarTodas(){
+		return movimentacoesRepository.findAll();
+	}
+	
+	public Conta alterarSaldo(Movimentacoes mov) {
+		
+		Conta conta = contaRepository.findOne(mov.getCliente().getId());
+		
+		if(mov.getTipo().getCod() == 1) {
+			
+			
+			
+			
+		}
+		
+		return null;
+	}
 }
